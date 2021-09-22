a46faad54587986df5814e1546f8220e7916e2c9
Jose Narvaez <josenarvaez@utez.edu.mx>
2021-09-22 13:37:59 -0500

    Se agrego archivo manifest y configuro para github

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 
 // Preguntamos si el navegador y/o el sitio dispone de SW
+let url = window.location.href;
+let swDirect = "/20213-PWA-U2-P1/sw.js";
+
 if(navigator.serviceWorker){
     console.log("Service Worker Disponible");
-    navigator.serviceWorker.register('/sw.js');
+    if(url.includes('localhost')){
+        swDirect='/sw.js';
+    }
+
+    navigator.serviceWorker.register(swDirect);
 }else{
     console.log('Service Worker NO Disponible');
 }
